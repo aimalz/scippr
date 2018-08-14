4ec8621eb7bdd1502f89ab0a16a77120b41d6577
renee.hlozek@gmail.com <renee.hlozek@gmail.com>
2018-08-14 12:32:14 -0500

    normalise functions

---
+++ code/demos/normalise_funs.py
@@ -0,0 +1,69 @@
+import numpy as np
+def normalize_t(arr, vb=False):
+    norm_factor = np.sum(arr)
+    arr /= norm_factor
+    var = np.sum(arr)
+    try:
+        assert np.isclose(var, 1.)
+        if vb: print(var)
+    except AssertionError:
+        print('normalization error '+str(var))
+    return arr
+    
+def normalize_z(arr, z_difs, vb=False):
+    norm_factor = np.sum(arr * z_difs)
+    arr /= norm_factor
+    var = np.sum(arr * z_difs)
+    try:
+        assert np.isclose(var, 1.)
+        if vb: print(var)
+    except AssertionError:
+        print('normalization error '+str(var))
+    return arr
+
+
+def normalize_zt(arr, z_difs, vb=False):
+    norm_factor = np.sum(arr * z_difs[np.newaxis, :])
+    arr /= norm_factor
+    var = np.sum(arr * z_difs[np.newaxis, :])
+    try:
+        assert np.isclose(var, 1.)
+        if vb: print(var)
+    except AssertionError:
+        print('normalization error '+str(var))
+    return arr
+
+def normalize_hubble(arr, z_difs, mu_difs, vb=False):
+    norm_factor = np.sum(arr * z_difs[:, np.newaxis] * mu_difs[np.newaxis, :])
+    arr /= norm_factor
+    var = np.sum(arr * z_difs[:, np.newaxis] * mu_difs[np.newaxis, :])
+    try:
+        assert np.isclose(var, 1.)
+        if vb: print(var)
+    except AssertionError:
+        print('normalization error '+str(var))
+    return arr
+    
+def normalize_one(arr,z_difs, mu_difs, vb=False):
+    norm_factor = np.sum(arr * z_difs[np.newaxis, :, np.newaxis] * mu_difs[np.newaxis, np.newaxis, :])
+    arr /= norm_factor
+    var = np.sum(arr * z_difs[np.newaxis, :, np.newaxis] * mu_difs[np.newaxis, np.newaxis, :])
+    try:
+        assert np.isclose(var, 1.)
+        if vb: print(var)
+    except AssertionError:
+        print('normalization error '+str(var))
+    return arr
+    
+def normalize_all(arr, z_difs, mu_difs, vb=False):
+    n_objs = len(arr)
+    norm_factor = np.sum(np.sum(np.sum(arr * z_difs[np.newaxis, np.newaxis, :, np.newaxis] * mu_difs[np.newaxis, np.newaxis, np.newaxis, :], axis=3), axis=2), axis=1)
+    arr /= norm_factor[:, np.newaxis, np.newaxis, np.newaxis]
+    var = np.sum(np.sum(np.sum(arr * z_difs[np.newaxis, np.newaxis, :, np.newaxis] * mu_difs[np.newaxis, np.newaxis, np.newaxis, :], axis=3), axis=2), axis=1)
+    try:
+        assert np.all(np.isclose(var, np.ones(n_objs)))
+        if vb: print(var)
+    except AssertionError:
+        print('normalization error '+str(var))
+    return arr
+    
